78820bd9a645d80a8a6c00368ba4afdf7efbc5f4
Rishov Sarkar <rishov.sarkar@gatech.edu>
2021-06-29 03:07:09 -0400

    Squashed commit of the following:
    commit 73ef3f67a3e93ae472c35e168eec78f308cff626
    Author: Rishov Sarkar <rishov.sarkar@gatech.edu>
    Date:   Tue Jun 29 03:02:13 2021 -0400
    
        Add basic rhino AI: fight and eat
    
    commit 26e79cd3df8c5833e7fca72b3948762c22bcae19
    Author: Rishov Sarkar <rishov.sarkar@gatech.edu>
    Date:   Tue Jun 29 01:07:09 2021 -0400
    
        Prepare to add rhino AI
    
    commit 31d110628c36d766d9e457bdface5eb1fec77553
    Author: Rishov Sarkar <rishov.sarkar@gatech.edu>
    Date:   Tue Jun 29 00:57:37 2021 -0400
    
        Add NavMeshComponents package
    
    commit ae56c78795a21d61ea273fc8f9252e5faee27385
    Author: Rishov Sarkar <rishov.sarkar@gatech.edu>
    Date:   Tue Jun 29 00:55:27 2021 -0400
    
        Add Packages/
    
        I think this got lost during a merge.

---
+++ Assets/Scripts/Utility/CameraAspectRatio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraAspectRatio : MonoBehaviour
+{
+    public float aspectRatio = 16f / 9f;
+    private new Camera camera;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        camera.aspect = aspectRatio;
+    }
+}
---
+++ Assets/Scripts/Utility/NavMeshAgentDeceleration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentDeceleration : MonoBehaviour
+{
+    public float deceleration = 60f;
+    private float acceleration;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        acceleration = agent.acceleration;
+    }
+
+    void Update()
+    {
+        if (agent.hasPath && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.acceleration = deceleration;
+        }
+        else
+        {
+            agent.acceleration = acceleration;
+        }
+    }
+}
