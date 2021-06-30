15b1f01b441920a7cf49a930e3b5f454b5340c26
Michael Song <wsong75@gatech.edu>
2021-06-29 23:02:58 -0400

    merge with new sounds

---
+++ Assets/Scripts/GUI/GameQuitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GameQuitter : MonoBehaviour
+{
+    private AudioSource clickAudio;
+    public void QuitGameWithSound()
+    {
+        StartCoroutine(btnClickSound());
+    }
+    IEnumerator btnClickSound()
+    {
+        clickAudio = GetComponent<AudioSource>();
+        clickAudio.Play();
+        yield return new WaitForSeconds(clickAudio.clip.length*0.2f);
+        QuitGame();
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
