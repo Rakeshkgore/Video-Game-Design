7e775d61080d5ab3fde4e942714ce8f61c321069
Gorrepati, Rakesh Krishna <rgorrepati2@gatech.edu>
2021-07-07 01:07:45 -0400

    Health Text on HUD

---
+++ Assets/EnemyHealthText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthText : MonoBehaviour
+{
+    private Text healthText;
+    public Transform player;
+    public GetHealth golemHealth;
+    public GetHealth rhinoHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Enemy Health: " + (player.position.y < -1 ? golemHealth.hp : rhinoHealth.hp);
+    }
+}
---
+++ Assets/PlayerHealthText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthText : MonoBehaviour
+{
+    private Text healthText;
+    public GetHealth playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Player Health: " + playerHealth.hp.ToString();
+    }
+}
