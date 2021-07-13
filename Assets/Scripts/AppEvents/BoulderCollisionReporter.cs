2e81f5ec708e3e6ae7431182430f91420eff8a2c
Yi Chin Chen <ychen3243@gatech.edu>
2021-07-12 22:05:01 -0400

    Update pause menu and audio

--- Assets/Scripts/AppEvents/BoulderCollisionReporter.cs
+++ Assets/Scripts/AppEvents/BoulderCollisionReporter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class StoneCollisionReporter : MonoBehaviour
+public class BoulderCollisionReporter : MonoBehaviour
 {
     void OnCollisionEnter(Collision c)
     {
