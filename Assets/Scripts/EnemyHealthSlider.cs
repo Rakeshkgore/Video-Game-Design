d8385ced757ac8c2f7debbfbfdba181645859c09
Rishov Sarkar <rishov.sarkar@gatech.edu>
2021-07-19 16:17:58 -0400

    New health bar layout
    Roughly inspired by Dark Souls

--- Assets/Scripts/EnemyHealthSlider.cs
+++ Assets/Scripts/EnemyHealthSlider.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 public class EnemyHealthSlider : MonoBehaviour
 {
     public Slider enemyHealthSlider;
+    public GameObject enemyHealthFillArea;
+    public Text enemyHealthText;
     public GetHealth golemHealth;
     public GetHealth rhinoHealth;
     public Transform player;
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +22,47 @@ public class EnemyHealthSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string currentEnemyName;
+        float currentEnemyHp;
+        float currentEnemyMaxHp;
+        bool showHealthBar;
+
+        if (player.position.x < -34.33073) // FIXME
+        {
+            currentEnemyName = "Rock Golem";
+            currentEnemyHp = golemHealth.hp;
+            currentEnemyMaxHp = golemHealth.maxHp;
+            showHealthBar = currentEnemyHp > 0f && (
+                currentEnemyHp < currentEnemyMaxHp
+                || IsVisibleToPlayer(golemHealth.transform.position)
+            );
+        }
+        else
+        {
+            currentEnemyName = "Rhino";
+            currentEnemyHp = rhinoHealth.hp;
+            currentEnemyMaxHp = rhinoHealth.maxHp;
+            showHealthBar = currentEnemyHp > 0f;
+        }
+
+        enemyHealthText.gameObject.SetActive(showHealthBar);
+        enemyHealthFillArea.SetActive(showHealthBar);
+        enemyHealthText.text = currentEnemyName;
         enemyHealthSlider.minValue = 0;
-        enemyHealthSlider.maxValue = player.position.x < -34.33073 ? golemHealth.maxHp : rhinoHealth.maxHp;
-        enemyHealthSlider.value = player.position.x < -34.33073 ? golemHealth.hp : rhinoHealth.hp;
+        enemyHealthSlider.maxValue = currentEnemyMaxHp;
+        enemyHealthSlider.value = currentEnemyHp;
+    }
+
+    private bool IsVisibleToPlayer(Vector3 point)
+    {
+        Vector3 viewportPoint = mainCamera.WorldToViewportPoint(point);
+        return (
+            viewportPoint.x >= 0f
+            && viewportPoint.x <= 1f
+            && viewportPoint.y >= 0f
+            && viewportPoint.y <= 1f
+            && viewportPoint.z >= mainCamera.nearClipPlane
+            && viewportPoint.z <= mainCamera.farClipPlane
+        );
     }
 }
