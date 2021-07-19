5db5857e09c929b3049d4ac18e8b7c7bf3a2fdb5
Gorrepati, Rakesh Krishna <rgorrepati2@gatech.edu>
2021-07-19 15:20:24 -0400

    HealthBars for Enemy/Player

---
+++ Assets/Scripts/EnemyHealthSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyHealthSlider : MonoBehaviour
+{
+    public Slider enemyHealthSlider;
+    public GetHealth golemHealth;
+    public GetHealth rhinoHealth;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyHealthSlider = GetComponent<Slider>();
+
+        enemyHealthSlider.value = golemHealth.maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyHealthSlider.minValue = 0;
+        enemyHealthSlider.maxValue = player.position.x < -34.33073 ? golemHealth.maxHp : rhinoHealth.maxHp;
+        enemyHealthSlider.value = player.position.x < -34.33073 ? golemHealth.hp : rhinoHealth.hp;
+    }
+}
---
+++ Assets/Scripts/PlayerHealthSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthSlider : MonoBehaviour
+{
+    public Slider playerHealthSlider;
+    public GetHealth playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealthSlider = GetComponent<Slider>();
+        playerHealthSlider.value = playerHealth.hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealthSlider.value = playerHealth.hp;
+    }
+}
