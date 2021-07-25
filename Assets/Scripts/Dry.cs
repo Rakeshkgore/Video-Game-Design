78066f093be673e96ac2d72a616707e37d89cf68
Michael Song <wsong75@gatech.edu>
2021-07-25 02:15:28 -0400

    puzzles finished

---
+++ Assets/Scripts/Dry.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dry : MonoBehaviour
+{
+    private Animator anim;
+    GameObject rb;
+
+    void Awake()
+    {
+        anim = this.transform.parent.gameObject.GetComponent<Animator>();
+        rb = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        GetBlessed gb = rb.GetComponent<GetBlessed>();
+        if (gb.PoseidonPassed)
+        {
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = new Vector3 (2.62f, 0f, 40.72f);
+        }
+    }
+}
---
+++ Assets/Scripts/MazeGone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGone : MonoBehaviour
+{
+    Animator anim;
+    GameObject rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetBlessed gb = rb.GetComponent<GetBlessed>();
+        if (gb.DaedalusPassed)
+        {
+            anim.SetTrigger("Down");
+        }
+    }
+}
