a870245a38a6f3c68f3c08cd424d990adf22bb3b
Yi Chin Chen <ychen3243@gatech.edu>
2021-07-27 19:52:58 -0400

    Add more sound effects

---
+++ Assets/Scripts/AppEvents/DescendingWallSoundEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DescendingWallSoundEffect : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip descendingWallAudio;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayDescendingWall()
+    {
+        audioSource.PlayOneShot(descendingWallAudio, 1f);
+    }
+}
