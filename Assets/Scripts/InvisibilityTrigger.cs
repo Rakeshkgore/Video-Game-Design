5cefd8ba9b064c513378533aa7882ee6da671174
Rishov Sarkar <rishov.sarkar@gatech.edu>
2021-07-27 22:35:51 -0400

    Initial framework for invisibility

---
+++ Assets/Scripts/InvisibilityTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibilityTrigger : MonoBehaviour
+{
+    private int triggerCount = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GetBlessed gb;
+        Invisibility invisibility;
+
+        if (other.attachedRigidbody == null
+            || !other.attachedRigidbody.TryGetComponent<GetBlessed>(out gb)
+            || !other.attachedRigidbody.TryGetComponent<Invisibility>(out invisibility))
+        {
+            return;
+        }
+
+        if (triggerCount == 0 && gb.DaedalusPassed)
+        {
+            invisibility.SetInvisible(true);
+        }
+        ++triggerCount;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GetBlessed gb;
+        Invisibility invisibility;
+
+        if (other.attachedRigidbody == null
+            || !other.attachedRigidbody.TryGetComponent<GetBlessed>(out gb)
+            || !other.attachedRigidbody.TryGetComponent<Invisibility>(out invisibility))
+        {
+            return;
+        }
+
+        --triggerCount;
+        if (triggerCount == 0 && gb.DaedalusPassed)
+        {
+            invisibility.SetInvisibleFor(invisibility.duration);
+        }
+    }
+}
